e978582572fb19d4ce10109ffc1881575537d60c
scottsutherland <scottsutherland@users.noreply.github.com>
2014-10-08 19:18:45 -0500

    asdf

---
+++ program.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+ $("#openall").click(function(){
+ 	$("#panelbox div.panel").slideDown("slow");
+ });
+});
+
+$(document).ready(function(){
+ $("#closeall").click(function(){
+ 	$("#panelbox div.panel").slideUp("slow");
+ });
+});
+
+
+
+
+
+
+
